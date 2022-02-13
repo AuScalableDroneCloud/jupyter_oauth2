2102aa5a0c9c04897ac17278d635af03955c7fbd
Owen Kaluza <owen.kaluza@monash.edu>
2022-02-13 15:28:48 +1100

    Fix PIL import test

--- jupyter_oauth2_api.py
+++ jupyter_oauth2_api.py
@@ -536,7 +536,7 @@ def device_connect(config=None, qrcode=True, scope=""):
         try:
             import io
             import qrcode
-            import pillow
+            from PIL import Image
         except (ImportError) as e:
             qrcode = False
             pass
