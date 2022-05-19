7daf9b862547fb656c3fdbb012709d9a86cc48f2
Owen Kaluza <owen.kaluza@monash.edu>
2022-05-19 12:43:00 +1000

    Fix redirect and remove indent from base templates

--- jupyter_oauth2_custom.py
+++ jupyter_oauth2_custom.py
@@ -1,4 +1,60 @@
-#This is a placeholder
+py_base = """
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#   kernel_info:
+#     name: python3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + [markdown] inputHidden=false outputHidden=false
+# # Loading a data set from ASDC WebODM
+#
+# This notebook / script will load a specific task dataset
+#
+
+# + inputHidden=false outputHidden=false
+import asdc
+
+await asdc.auth.connect(mode='iframe')
+
+project = '{PID}'
+task = '{TID}'
+filename = '{ASSET}'
+asdc.download(project, task, asset)
+
+"""
+
+html_base = """
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>ASDC API server</title>
+</head>
+
+<script>
+{script}
+</script>
+
+<body>
+    <h1>ASDC API Request</h3>
+    <p>Request processed for {FN}
+    <a href="/user-redirect/lab/tree/{FN}">(Output here)</a>
+    </p>
+</body>
+
+</html>
+"""
+
 def handler(request):
     """
     Additional functions can be added here
@@ -16,65 +72,14 @@ def handler(request):
     redirect = request.get_argument('redirect', 'yes')
     filename = 'task_{0}.py'.format(task)
 
-    content = """
-    # ---
-    # jupyter:
-    #   jupytext:
-    #     text_representation:
-    #       extension: .py
-    #       format_name: light
-    #       format_version: '1.5'
-    #   kernel_info:
-    #     name: python3
-    #   kernelspec:
-    #     display_name: Python 3
-    #     language: python
-    #     name: python3
-    # ---
-
-    # + [markdown] inputHidden=false outputHidden=false
-    # # Loading a data set from ASDC WebODM
-    #
-    # This notebook / script will load a specific task dataset
-    #
-
-    # + inputHidden=false outputHidden=false
-    import asdc
-
-    await asdc.auth.connect(mode='iframe')
-
-    project = '{PID}'
-    task = '{TID}'
-    filename = '{ASSET}'
-    asdc.download(project, task, asset)
-
-    """.format(PID=project, TID=task, ASSET=asset)
-
+    # Write the python script / notebook
     from pathlib import Path
     with open(str(Path.home() / filename), 'w') as f:
-        f.write(content)
+        f.write(py_base.format(PID=project, TID=task, ASSET=asset))
 
+    script = ""
     if redirect == 'yes':
-        #self.redirect("/user-redirect/lab/tree/" + filename)
-        self.redirect("https://jupyter.asdc.cloud.edu.au/user-redirect/lab/tree/" + filename)
-    else:
-        request.write("""
-        <!DOCTYPE html>
-        <html lang="en">
+        script = 'window.location.href="/user-redirect/lab/tree/{FN}"'.format(FN=filename)
 
-        <head>
-            <meta charset="utf-8" />
-            <title>ASDC API server</title>
-        </head>
-
-        <body>
-            <h1>ASDC API Request</h3>
-            <p>Request processed for {FN}
-            <a href="https://jupyter.asdc.cloud.edu.au/user-redirect/lab/tree/{FN}">(Output here)</a>
-            <a href="/user-redirect/lab/tree/{FN}">(Output here)</a>
-            </p>
-        </body>
-
-        </html>
-        """.format(FN=filename))
+    request.write(html_base.format(FN=filename, script=script))
 
